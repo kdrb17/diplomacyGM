66c69b6769301b13e45f99565318f16dc78bc206
kdrb17 <kevin@brosnanweb.net>
2024-11-06 20:25:42 +0000

    test202041106

--- diplomacyGM.py
+++ diplomacyGM.py
@@ -1,5 +1,8 @@
 import sqlite3
 con = sqlite3.connect("diplomacyGM.db")
 cur = con.cursor()
-for row in cur.execute("SELECT provcode, provname FROM provinces ORDER BY provcode"):
-    print(row)
+for row in cur.execute("SELECT sprov, eprov FROM orderscur"):
+    var1 = row[0]
+    print(var1)
+    var2 = row[1]
+    print(var2)
