b839ecbb34ef00fc7cec2e71f028e2fbb909a8dc
kdrb17 <kevin@brosnanweb.net>
2024-11-06 21:59:23 +0000

    test20241106-3

--- diplomacyGM.py
+++ diplomacyGM.py
@@ -1,14 +1,14 @@
 import sqlite3
 con = sqlite3.connect("diplomacyGM.db")
 cur = con.cursor()
-for row in cur.execute("SELECT sprov, eprov FROM orderscur"):
-    var1 = row[0]
+for row1 in cur.execute("SELECT sprov, eprov FROM orderscur"):
+    var1 = row1[0]
     print(var1)
-    var2 = row[1]
+    var2 = row1[1]
     print(var2)
-var3 = (var1 + var2)
-print(var3)
-var4 = (var3,)
-for row in cur.execute("SELECT adjcode FROM adj WHERE adjcode = ?", (var4)):
-    var5 = row[0]
-    print(var5)
+    var3 = (var1 + var2)
+    print(var3)
+    var4 = (var3,)
+    for row2 in cur.execute("SELECT adjcode FROM adj WHERE adjcode = ?", (var4)):
+        var5 = row2[0]
+        print(var5)
