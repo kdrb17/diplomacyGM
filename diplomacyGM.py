b3d54cc7e670d5bbeaae09dd2579c10cd97a31f5
kdrb17 <kevin@brosnanweb.net>
2024-11-07 21:26:54 +0000

    20241107a

--- diplomacyGM.py
+++ diplomacyGM.py
@@ -1,14 +1,25 @@
+def checkadj():
+    for row2 in cur.execute("SELECT adjcode FROM adj WHERE adjcode = ?", (var4)):
+        var5 = row2[0]
+        print("checkadj-OK:", var5)
+
 import sqlite3
 con = sqlite3.connect("diplomacyGM.db")
 cur = con.cursor()
-for row1 in cur.execute("SELECT sprov, eprov FROM orderscur"):
-    var1 = row1[0]
+
+cur.execute("SELECT sprov, eprov FROM orderscur")
+records = cur.fetchall()
+print("Total rows are:  ", len(records))
+print("Printing each row")
+for row in records:
+    var1 = row[0]
     print(var1)
-    var2 = row1[1]
+    var2 = row[1]
     print(var2)
     var3 = (var1 + var2)
     print(var3)
     var4 = (var3,)
-    for row2 in cur.execute("SELECT adjcode FROM adj WHERE adjcode = ?", (var4)):
-        var5 = row2[0]
-        print(var5)
+    print(var4)
+    checkadj()
+
+cur.close()
