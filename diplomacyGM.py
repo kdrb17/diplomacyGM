0e4d797757ed96e63c8dea4e646c7c9f4796af5b
kdrb17 <kevin@brosnanweb.net>
2025-10-24 16:01:52 +0100

    fr-20252024-1600

--- diplomacyGM.py
+++ diplomacyGM.py
@@ -1,21 +1,14 @@
 def check_adj():
-    adjTEST = False
-    while adjTEST==False:
-        for row2 in cur.execute("SELECT adjcode FROM adjM"):
-            var5 = row2[0]
-            if var5 == var4:
-                adjTEST = True   
-    if adjTEST == True:
-        print("checkadj-OrderIsValid", var4)
-    else:
-        print("checkadj-OrderIs-NOT-Valid", var4)
+    cur2.execute("SELECT adjcode FROM adjM WHERE adjcode = ?", (var4)")
+    
+    
 
 import sqlite3
 con = sqlite3.connect("diplomacyGM.db")
-cur = con.cursor()
+cur1 = con.cursor()
 
-cur.execute("SELECT sprov, eprov FROM orderscur")
-records = cur.fetchall()
+cur1.execute("SELECT sprov, eprov FROM orderscur")
+records = cur1.fetchall()
 print("Total rows are:  ", len(records))
 print("Printing each row")
 for row in records:
@@ -25,8 +18,7 @@ for row in records:
     print(var2)
     var3 = (var1 + var2)
     print(var3)
-    var4 = (var3,)
-    print(var4)
+    var4 = (var3, )
     check_adj()
 
-cur.close()
+cur1.close()
